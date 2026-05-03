1620ff1704ba9f7e9da8a11cbf75e48702ee5189
kitsuyui <kitsuyui@kitsuyui.com>
2026-05-04 04:33:01 +0900

    Load packaged renderer from app bundle

--- main-src/libs/constants/paths.js
+++ main-src/libs/constants/paths.js
@@ -2,8 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 const path = require('path');
+const { app } = require('electron');
 
-const REACT_PATH = process.env.NODE_ENV === 'production'
+const REACT_PATH = app.isPackaged
   ? `file://${path.resolve(__dirname, 'index.html')}`
   : 'http://localhost:3000';
 
