00ae2fd01e8d71280888f95c97c22939c07642be
kitsuyui <kitsuyui+github@kitsuyui.com>
2026-06-19 11:15:09 +0900

    Add 30-second default timeout to customizedFetch via AbortSignal

--- main-src/libs/customized-fetch.ts
+++ main-src/libs/customized-fetch.ts
@@ -1,6 +1,11 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-const customizedFetch = (url: RequestInfo | URL, opts?: RequestInit) => fetch(url, opts);
+const DEFAULT_TIMEOUT_MS = 30_000;
+
+const customizedFetch = (url: RequestInfo | URL, opts?: RequestInit) => {
+  const signal = opts?.signal ?? AbortSignal.timeout(DEFAULT_TIMEOUT_MS);
+  return fetch(url, { ...opts, signal });
+};
 
 export default customizedFetch;
